3779389fce0ba09a3cdaa6d90f59eb8c3a1f5baa
Toru Yamaguchi <toruyama25@icloud.com>
2025-02-16 22:47:15 +0000

    updated .gitignore .gitattributes

--- Aurora/Intermediate/Build/Win64/x64/AuroraEditor/Development/Core/SharedPCH.Core.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Core/Public/CoreSharedPCH.h
-#include "C:/Users/toruy_iu/Documents/Y3 Modules/FYP/AuroraSim/Aurora/Intermediate/Build/Win64/x64/AuroraEditor/Development/Core/SharedDefinitions.Core.Cpp20.h"
-#include "Runtime/Core/Public/CoreSharedPCH.h"
--- Aurora/Intermediate/Build/Win64/x64/AuroraEditor/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h
-#include "C:/Users/toruy_iu/Documents/Y3 Modules/FYP/AuroraSim/Aurora/Intermediate/Build/Win64/x64/AuroraEditor/Development/CoreUObject/SharedDefinitions.CoreUObject.Cpp20.h"
-#include "Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h"
