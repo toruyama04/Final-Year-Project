9302571d067585081d3ce2a63712b9faa291ed98
Toru Yamaguchi <toruyama25@icloud.com>
2025-07-17 13:28:12 +0100

    Adding report

--- Aurora/Source/Aurora/Aurora.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Aurora.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aurora, "Aurora" );
